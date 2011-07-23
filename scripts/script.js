c2851e1a8bc32e29c1a8ce48f657ad1671a78513
Rico Sta. Cruz <rico@ambiescent.com>
2011-07-24 05:33:22 +0800

    .

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+(function($) {
+  /*$('a[href^=#]').live('click', function() {
+    var href = $(this).attr('href');
+
+    // Activate the link
+    $('a[href^=#]').removeClass('active');
+    $('a[href='+href+']').addClass('active');
+
+    // Activate the section
+    $('section.code').hide();
+    $(href).show();
+
+    return false;
+  });*/
+})(jQuery);
